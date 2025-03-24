17e1bda17e29e80f27940456dad649a2c4c56a92
jayanth2410 <jayanth24102003@gmail.com>
2025-03-24 18:59:00 +0530

    changes made in /analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,21 +20,24 @@ const App = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<StudentDashboard />} />
-      <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/" element={<StudentDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/complaint" element={<ComplaintForm />} />
         <Route path="/admin-complaints" element={<AdminComplaints />} />
         <Route path="/emergency-alert" element={<EmergencyAlert />} />
-        <Route path="/admin-emergency-alerts" element={<AdminEmergencyAlerts />} />
+        <Route
+          path="/admin-emergency-alerts"
+          element={<AdminEmergencyAlerts />}
+        />
         <Route path="/admin-notices" element={<AdminNotice />} />
         <Route path="/student-notices" element={<StudentNotices />} />
         <Route path="/sos" element={<SOS />} />
-        <Route path="/anal" element={<Dashboard/>}/>
-        <Route path="/login" element={<AuthPage/>}/>
+        <Route path="/analytics" element={<Dashboard />} />
+        <Route path="/login" element={<AuthPage />} />
         <Route path="/geo-tracking" element={<GeoTracking />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/admin-sos" element={<AdminSOS />} />
-        <Route path="/chat" element={<FloatingChatBot />}/>
+        <Route path="/chat" element={<FloatingChatBot />} />
       </Routes>
     </Router>
   );
